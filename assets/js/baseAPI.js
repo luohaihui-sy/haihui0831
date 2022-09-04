18d9a20e1e09a569c2d241a95d8d67fae599fcb9
louhaihui-sy <1160944876@qq.com>
2022-09-04 14:28:32 +0800

    完成个人中心的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -12,13 +12,13 @@ $.ajaxPrefilter(function(options) {
   }
 
   //全局统一挂载 complete 回调函数
-  options.complete = function(res) {
-    //在 complete 回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
-    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-      //1、强制清空 token
-      localStorage.removeItem('token')
-      //2、强制跳转到登录页面
-      location.href = '/login.html'
-    }
-  }
+  // options.complete = function(res) {
+  //   //在 complete 回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+  //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+  //     //1、强制清空 token
+  //     localStorage.removeItem('token')
+  //     //2、强制跳转到登录页面
+  //     location.href = '/login.html'
+  //   }
+  // }
 })
